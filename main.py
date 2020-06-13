1791238413c3bec4163038b6f188a67440d93ac6
Histefanhere <stefan.zdra@gmail.com>
2020-06-13 13:50:44 +1200

    GUI Editor Most of the GUI part of this is actually already done - however, I'm stuck with how I can re-parse it through the config parser.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 import subprocess, argparse, os, yaml, time
 
 parser = argparse.ArgumentParser(description="Quickly Run your favourite scripts and applications.\nFor aditional help, check the README.")
@@ -73,8 +74,8 @@ class Config():
 
         print("And that's it! Try to run this script without any arguments now, and thank you for using quickscripts! :)")
 
-                with open(self.get_file('config.yaml'), 'w+') as file:
-                    file.write(yaml.dump(data))
+        with open(self.get_file('config.yaml'), 'w+') as file:
+            file.write(yaml.dump(data))
 
         # We don't want to run the code when the user is setting an option
         exit()
@@ -131,7 +132,7 @@ class Config():
                 "name": groupname,
                 "links": grouplinks
             })
-
+        
         return groups
 
 config = Config()
@@ -173,6 +174,8 @@ class MainMenu(tk.Frame):
     def __init__(self, root):
         tk.Frame.__init__(self, root, padx=20, pady=20, bg=bg)
         self.root = root
+
+        self.editting = False
 
         self.labels = []
         # This is the link group that is currently active
@@ -249,6 +252,31 @@ class MainMenu(tk.Frame):
         # Call the recursive timer method which will count down
         self.quit_timer()
 
+        ### EDIT MENU LABEL
+        edit_config_label = tk.Label(self,
+            text=";",
+            font=("helvetica", 18),
+            anchor="e",
+            bg=bg, fg="grey"
+        )
+        edit_config_label.grid(row=min(i+2, n+3), column=0, sticky="E", pady=5)
+        self.labels.append(edit_config_label)
+        edit_config_button = tk.Button(self,
+            text="Edit Config",
+            fg=fg_deselect,
+            font=("helvetica", 12),
+            bg=bg,
+            highlightthickness=1,
+            highlightcolor=fg_deselect,
+            highlightbackground=fg_deselect,
+            borderwidth=0,
+            command=self.edit_menu
+        )
+        edit_config_button.grid(row=min(i+2, n+3), column=1, sticky="W", padx=5)
+        self.labels.append(edit_config_button)
+
+
+        ### CATEGORIES
         self.cat_row = tk.Frame(self, bg=bg)
         self.cat_row.grid(row=0, column=0, columnspan=99, sticky="nsew")
 
@@ -272,6 +300,11 @@ class MainMenu(tk.Frame):
             label.grid(row=0, column=2*j+1, sticky="nsw")
             self.labels.append(label)
             j += 1
+        
+    def edit_menu(self):
+        self.editting = True
+        self.newWindow = tk.Toplevel(self.master)
+        self.app = EditMenu(self.newWindow)
 
     def quit_timer(self):
         # 20 and division by 2 so that the timer goes twice as slow - actually reduces stress!
@@ -281,7 +314,210 @@ class MainMenu(tk.Frame):
             exit()
         if f"QUIT ({new_time})" != self.quit.get():
             self.quit.set(f"QUIT ({new_time})")
-        self.root.after(100, self.quit_timer)
+        if not self.editting:
+            self.root.after(100, self.quit_timer)
+
+
+class EditMenu(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master, padx=20, pady=20)
+        self.master = master
+
+        # self.quitButton = tk.Button(self, text = 'Quit', width = 25, command = self.close_windows)
+        # self.quitButton.grid(row=0, column=0)
+
+        # Group selection
+        self.selected_group = -1
+        tk.Label(self, text="Group Selection", anchor=tk.W, justify=tk.LEFT).grid(row=0, column=0)
+        self.group_combobox = ttk.Combobox(
+            self,
+            values=[x['name'] for x in config.groups],
+            state="readonly"
+        )
+        self.group_combobox.bind("<<ComboboxSelected>>", self.select_group)
+        self.group_combobox.grid(row=1, column=0)
+
+        # Script selection
+        self.script_frame = tk.Frame(self)
+        self.selected_script = -1
+        tk.Label(self, text="Script Selection:", anchor=tk.W, justify=tk.LEFT).grid(row=2, column=0)
+        self.script_listbox = tk.Listbox(
+            self.script_frame,
+            selectmode=tk.SINGLE
+            # values=[x['name'] for x in config.groups],
+            # state="readonly"
+        )
+        # Allow double click for picking a script
+        self.script_listbox.bind("<Double-Button-1>", self.select_script)
+        self.script_frame.grid(row=3, column=0, sticky=tk.W + tk.E + tk.N)
+        self.script_listbox.pack()
+
+        tk.Button(self.script_frame, text="Edit Script", command=self.select_script).pack(fill=tk.X)
+
+
+        self.script_vars = {
+            "name": tk.StringVar(),
+            "key": tk.StringVar(),
+            "cmd": None
+        }
+
+
+        script_config_frame = tk.Frame(self, padx=5)
+        tk.Label(script_config_frame, text="Script Configuration", justify=tk.CENTER).grid(
+            row=0, column=0
+        )
+
+        option_frame = tk.Frame(script_config_frame)
+
+        # Name option
+        tk.Label(
+            option_frame,
+            text="Name:",
+            justify=tk.LEFT
+        ).grid(row=0, column=0, sticky=tk.W)
+        tk.Entry(
+            option_frame,
+            textvariable=self.script_vars['name']
+        ).grid(row=0, column=1, sticky=tk.W)
+
+        # Key option
+        tk.Label(
+            option_frame,
+            text="Key:",
+            justify=tk.LEFT
+        ).grid(row=1, column=0, sticky=tk.W)
+        def strip(*args):
+            self.script_vars['key'].set(self.script_vars['key'].get()[:1])
+        self.script_vars['key'].trace("w", strip)
+        tk.Entry(
+            option_frame,
+            textvariable=self.script_vars['key'],
+            width=2
+        ).grid(row=1, column=1, sticky=tk.W)
+
+        option_frame.grid(row=1, column=0)
+
+        tk.Label(
+            script_config_frame,
+            text="Commands:",
+            justify=tk.CENTER
+        ).grid(row=2, column=0)
+
+        self.cmds_frame = tk.Frame(script_config_frame)
+        self.cmds_frames = []
+        self.show_cmds("default command")
+        self.cmds_frame.grid(row=3, column=0)
+
+        def save():
+            pass
+
+        tk.Button(
+            script_config_frame,
+            text="Save",
+            command=save
+        ).grid(row=5, column=0)
+
+        script_config_frame.grid(row=0, column=1, rowspan=4, sticky=tk.N + tk.S)
+
+        tk.Button(
+            self,
+            text="EXIT",
+            command=self.close_windows,
+            pady=2
+        ).grid(row=100, column=0, columnspan=2, sticky=tk.W + tk.E)
+
+
+        self.grid(row=0, column=0)
+
+        # https://www.python-course.eu/tkinter_dialogs.php
+        # https://stackoverflow.com/questions/41946222/how-do-i-create-a-popup-window-in-tkinter
+    
+    def show_cmds(self, cmds):
+        """Based on the `cmds` input, manages if a single cmd or multiple are needed"""
+        for cmd in self.cmds_frames:
+            cmd.pack_forget()
+        self.cmds_frames = []
+        if isinstance(cmds, str):
+            # just a single command
+            cmd_frame = tk.Frame(
+                self.cmds_frame,
+            )
+            label = tk.Label(
+                cmd_frame,
+                text="Name:",
+                justify=tk.LEFT
+            )
+            label.grid(row=0, column=0, sticky=tk.W)
+            entry = tk.Entry(
+                cmd_frame,
+            )
+            entry.grid(row=0, column=1, sticky=tk.W)
+            cmd_frame.pack()
+            self.cmds_frames.append(cmd_frame)
+
+        elif isinstance(cmds, list):
+            # list of (name, cmd) tuples
+            pass
+
+        # for i in range(2):
+        #     single_cmd_frame = tk.Frame(
+        #         self.cmds_frame,
+        #         pady=0 if i == 0 else 10
+        #     )
+        #     tk.Label(
+        #         single_cmd_frame,
+        #         text="Name:",
+        #         justify=tk.LEFT
+        #     ).grid(row=0, column=0, sticky=tk.W)
+        #     tk.Entry(
+        #         single_cmd_frame,
+        #     ).grid(row=0, column=1, sticky=tk.W)
+
+        #     tk.Label(
+        #         single_cmd_frame,
+        #         text="Cmd:",
+        #         justify=tk.LEFT
+        #     ).grid(row=1, column=0, sticky=tk.W)
+        #     tk.Entry(
+        #         single_cmd_frame,
+        #     ).grid(row=1, column=1, sticky=tk.W)
+
+        #     single_cmd_frame.pack()
+    
+    def select_group(self, event):
+        new_selection = self.group_combobox.current()
+        if not self.selected_group == new_selection:
+            # new selection
+            self.selected_group = new_selection
+
+            # self.script_combobox['values'] = [link[0] for link in config.groups[self.selected_group]['links']]
+            # self.script_combobox.current(0)]
+            self.script_listbox.delete(0, tk.END)
+            for link in [link[0] for link in config.groups[self.selected_group]['links']]:
+                self.script_listbox.insert(tk.END, link)
+    
+    def select_script(self, event=None):
+        # new_selection = self.script_combobox.current()
+        new_selection = self.script_listbox.curselection()[0]
+        if not self.selected_script == new_selection:
+            # new selection
+            self.selected_script = new_selection
+            self.display_script()
+    
+    def display_script(self):
+        script = config.groups[self.selected_group]['links'][self.selected_script]
+
+        self.script_vars["name"].set(script[0])
+        self.script_vars["key"].set(script[1])
+
+        # needs to be based on the type of script[2]
+        # self.script_vars["cmd"].set(script[2])
+
+    
+    def close_windows(self):
+        app.editting = False
+        self.master.destroy()
+
 
 root = tk.Tk()
 root.title("quickscripts")
@@ -292,10 +528,13 @@ def parse_key(event=None, key=None):
         print(key)
         char = key
     else:
-    char = event.char
+        char = event.char
 
     if char == "q":
         root.destroy()
+    
+    if char == ";":
+        app.edit_menu()
 
     if char in "123456789":
         app.set_active_group(int(char))
