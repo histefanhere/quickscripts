3d030c4223d89b9e2ee1e705235cb5d8037ac205
Histefanhere <stefan.zdra@gmail.com>
2024-02-28 10:11:11 +1300

    🚧 Migrate frameworks

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,41 @@
 
+var active_group = null;
+var groups = [];
+var scripts = [];
+
+
+function setActiveGroup(group) {
+    if (group === active_group) {
+        return;
+    }
+    for (var i = 0; i < groups.length; i++) {
+        if (groups[i].key == group) {
+            active_group = group;
+            scripts = groups[i].scripts;
+            // TODO: update the UI to reflect the new active group
+            console.log('Active group: ' + active_group);
+            return;
+        }
+    }
+}
+
+
 // Print to the console whenever the letter Q is pressed
 document.addEventListener('keydown', function(event) {
     if (event.key.toLowerCase() === 'q') {
-        pywebview.api.close(event.shiftKey);
+        pywebview.api.close();
     }
-    // else {
-    //     // I want the FRONTEND to determine what (or if) script should be executed and call a
-    //     // `execute_script` method on the backend.
-    //     // This is just a placeholder
-    //     pywebview.api.parse_key(event.key, event.shiftKey)
-    // }
+    else {
+        // pywebview.api.parse_key(event.key, event.shiftKey)
+        for (var i = 0; i < scripts.length; i++) {
+            if (scripts[i].key === event.key) {
+                console.log(scripts[i].script);
+                pywebview.api.execute(scripts[i].command);
+            }
+        }
+    }
 });
+
 
 // var countdown = 10;
 var countdown = 100;
@@ -18,14 +43,21 @@ setInterval(function() {
     countdown--;
     document.getElementById('countdown').innerText = countdown;
     if (countdown === 0) {
-        pywebview.api.close(false);
+        pywebview.api.close();
     }
 }, 2000);
 
-// TODO: figure out why this is still not the right size - a little too big
+
 window.addEventListener('pywebviewready', function() {
-    let width = document.body.scrollWidth;
-    let height = document.body.scrollHeight;
-    console.log(width); console.log(height);
-    pywebview.api.fit_window(width, height);
+    pywebview.api.get_groups().then(function(response) {
+        groups = response;
+        console.log('Groups: ' + groups);
+        setActiveGroup(1);
+        
+        // TODO: figure out why this is still not the right size - a little too big
+        let width = document.body.scrollWidth;
+        let height = document.body.scrollHeight;
+        console.log(width); console.log(height);
+        pywebview.api.fit_window(width, height);
+    });
 });
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import tkinter as tk
-from tkinter import messagebox
+# import tkinter as tk
+# from tkinter import messagebox
+import webview
 import subprocess, argparse, os, yaml, time
 
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 
 parser = argparse.ArgumentParser(description="Quickly Run your favourite scripts and applications.\nFor aditional help, check the README.")
 parser.add_argument("--configure", action="store_true", help="Set some options of the script")
@@ -11,8 +12,7 @@ args = parser.parse_args()
 valid_keys = "abcdefghijklmnoprstuvwxyz,.<>/?;:'\"[]{}-_=+!@#$%^&*()`~"
 invalid_keys = "q"
 
-def error(message, title="Error!"):
-    messagebox.showerror(title, message)
+def error(message):
     raise Exception(message)
 
 @dataclass
@@ -26,9 +26,7 @@ class Script:
     name: str
     key: str
     command: str
-    
-    def execute(self):
-        subprocess.Popen(self.command.split(), shell=True)
+
 
 class Config():
     def get_file(self, filename):
@@ -169,186 +167,195 @@ class Config():
 
         return groups
 
-class customButton(tk.Button):
-    def __init__(self, master, **kwargs):
-        tk.Button.__init__(self, master, **kwargs)
+# class customButton(tk.Button):
+#     def __init__(self, master, **kwargs):
+#         tk.Button.__init__(self, master, **kwargs)
 
-        self.bind("<Enter>", self.on_enter)
-        self.bind("<Leave>", self.on_leave)
+#         self.bind("<Enter>", self.on_enter)
+#         self.bind("<Leave>", self.on_leave)
 
-    def on_enter(self, event):
-        self['borderwidth'] = 1
-        self['padx'] = 0
-        # self['background'] = fg_deselect
+#     def on_enter(self, event):
+#         self['borderwidth'] = 1
+#         self['padx'] = 0
+#         # self['background'] = fg_deselect
     
-    def on_leave(self, event):
-        self['borderwidth'] = 0
-        self['padx'] = 1
-        # self['background'] = bg
+#     def on_leave(self, event):
+#         self['borderwidth'] = 0
+#         self['padx'] = 1
+#         # self['background'] = bg
 
-class MainMenu(tk.Frame):
-    def __init__(self, root):
-        tk.Frame.__init__(self, root, padx=20, pady=20, bg=bg)
-        self.root = root
+# class MainMenu(tk.Frame):
+#     def __init__(self, root):
+#         tk.Frame.__init__(self, root, padx=20, pady=20, bg=bg)
+#         self.root = root
 
-        self.labels = []
+#         self.labels = []
 
-        self.active_group = None
-        self.set_active_group(1)
+#         self.active_group = None
+#         self.set_active_group(1)
 
-        self.grid(row=0, column=0)
+#         self.grid(row=0, column=0)
 
-    def set_active_group(self, n):
-        if n == self.active_group:
-            return
-        for group in config.groups:
-            if group.key == n:
-                self.active_group = n
-                self.scripts = group.scripts
-                self.create_widgets()
+#     def set_active_group(self, n):
+#         if n == self.active_group:
+#             return
+#         for group in config.groups:
+#             if group.key == n:
+#                 self.active_group = n
+#                 self.scripts = group.scripts
+#                 self.create_widgets()
 
-    def create_widgets(self):
-        for label in self.labels:
-            label.grid_forget()
-        self.labels = []
+#     def create_widgets(self):
+#         for label in self.labels:
+#             label.grid_forget()
+#         self.labels = []
 
-        i = 0
-        n = config.get_option("rows", 5)
-        # Here we generate all the link lables. i = the ith link, and n = the number of links per column to show.
-        for script in self.scripts:
-            script_letter_label = tk.Label(
-                self,
-                text=script.key.upper(),
-                font=("helvetica", 18),
-                anchor="e",
-                bg=bg, fg=fg
-            )
-            script_letter_label.grid(row=(i%n)+1, column=(i//n)*2, sticky="E", pady=5)
-            self.labels.append(script_letter_label)
-            script_name_label = customButton(
-                self,
-                text=script.name,
-                font=("helvetica", 12),
-                bg=bg, fg=fg,
-                borderwidth=0,
-                padx=1,
-                command=lambda key_=script.key: parse_key(key=key_)
-            )
-            script_name_label.grid(row=(i%n)+1, column=1+(i//n)*2, sticky="W", padx=5)
-            self.labels.append(script_name_label)
-            i += 1
+#         i = 0
+#         n = config.get_option("rows", 5)
+#         # Here we generate all the link lables. i = the ith link, and n = the number of links per column to show.
+#         for script in self.scripts:
+#             script_letter_label = tk.Label(
+#                 self,
+#                 text=script.key.upper(),
+#                 font=("helvetica", 18),
+#                 anchor="e",
+#                 bg=bg, fg=fg
+#             )
+#             script_letter_label.grid(row=(i%n)+1, column=(i//n)*2, sticky="E", pady=5)
+#             self.labels.append(script_letter_label)
+#             script_name_label = customButton(
+#                 self,
+#                 text=script.name,
+#                 font=("helvetica", 12),
+#                 bg=bg, fg=fg,
+#                 borderwidth=0,
+#                 padx=1,
+#                 command=lambda key_=script.key: parse_key(key=key_)
+#             )
+#             script_name_label.grid(row=(i%n)+1, column=1+(i//n)*2, sticky="W", padx=5)
+#             self.labels.append(script_name_label)
+#             i += 1
 
-        ### QUIT BUTTON
-        self.quit = tk.StringVar()
-        self.quit.set("QUIT (10)")
-        self.start_time = time.time()
-        quit_label = tk.Label(self,
-            text="Q",
-            font=("helvetica", 18),
-            anchor="e",
-            bg=bg, fg="red"
-        )
-        quit_label.grid(row=min(i+1, n+2), column=0, sticky="E", pady=5)
-        self.labels.append(quit_label)
-        quit_button = tk.Button(self,
-            textvariable=self.quit,
-            fg="red",
-            font=("helvetica", 12),
-            bg=bg,
-            borderwidth=0,
-            command=root.destroy
-        )
-        quit_button.grid(row=min(i+1, n+2), column=1, sticky="W", padx=5)
-        self.labels.append(quit_button)
-        # Call the recursive timer method which will count down
-        self.quit_timer()
+#         ### QUIT BUTTON
+#         self.quit = tk.StringVar()
+#         self.quit.set("QUIT (10)")
+#         self.start_time = time.time()
+#         quit_label = tk.Label(self,
+#             text="Q",
+#             font=("helvetica", 18),
+#             anchor="e",
+#             bg=bg, fg="red"
+#         )
+#         quit_label.grid(row=min(i+1, n+2), column=0, sticky="E", pady=5)
+#         self.labels.append(quit_label)
+#         quit_button = tk.Button(self,
+#             textvariable=self.quit,
+#             fg="red",
+#             font=("helvetica", 12),
+#             bg=bg,
+#             borderwidth=0,
+#             command=root.destroy
+#         )
+#         quit_button.grid(row=min(i+1, n+2), column=1, sticky="W", padx=5)
+#         self.labels.append(quit_button)
+#         # Call the recursive timer method which will count down
+#         self.quit_timer()
 
-        ### GROUPS
-        self.group_row = tk.Frame(self, bg=bg)
-        self.group_row.grid(row=0, column=0, columnspan=99, sticky="nsew")
+#         ### GROUPS
+#         self.group_row = tk.Frame(self, bg=bg)
+#         self.group_row.grid(row=0, column=0, columnspan=99, sticky="nsew")
 
-        i = 0
-        for group in config.groups:
-            label = tk.Label(self.group_row,
-                text=str(group.key),
-                padx=5,
-                font=("helvetica", 18),
-                fg=fg if group.key == self.active_group else fg_deselect,
-                bg=bg
-            )
-            label.grid(row=0, column=2*i, sticky="nse")
-            self.labels.append(label)
-            label = customButton(self.group_row,
-                text=group.name,
-                font=("helvetica", 12),
-                fg=fg if group.key == self.active_group else fg_deselect,
-                bg=bg,
-                borderwidth=0,
-                padx=1,
-                command=lambda key_=group.key: parse_key(key=str(key_))
-            )
-            label.grid(row=0, column=2*i+1, sticky="nsw")
-            self.labels.append(label)
-            i += 1
+#         i = 0
+#         for group in config.groups:
+#             label = tk.Label(self.group_row,
+#                 text=str(group.key),
+#                 padx=5,
+#                 font=("helvetica", 18),
+#                 fg=fg if group.key == self.active_group else fg_deselect,
+#                 bg=bg
+#             )
+#             label.grid(row=0, column=2*i, sticky="nse")
+#             self.labels.append(label)
+#             label = customButton(self.group_row,
+#                 text=group.name,
+#                 font=("helvetica", 12),
+#                 fg=fg if group.key == self.active_group else fg_deselect,
+#                 bg=bg,
+#                 borderwidth=0,
+#                 padx=1,
+#                 command=lambda key_=group.key: parse_key(key=str(key_))
+#             )
+#             label.grid(row=0, column=2*i+1, sticky="nsw")
+#             self.labels.append(label)
+#             i += 1
 
-    def quit_timer(self):
-        # 20 and division by 2 so that the timer goes twice as slow - actually reduces stress!
-        new_time = round((20 - time.time() + self.start_time)/2)
-        if new_time == -1:
-            self.root.destroy()
-            exit()
-        if f"QUIT ({new_time})" != self.quit.get():
-            self.quit.set(f"QUIT ({new_time})")
-        self.root.after(100, self.quit_timer)
+#     def quit_timer(self):
+#         # 20 and division by 2 so that the timer goes twice as slow - actually reduces stress!
+#         new_time = round((20 - time.time() + self.start_time)/2)
+#         if new_time == -1:
+#             self.root.destroy()
+#             exit()
+#         if f"QUIT ({new_time})" != self.quit.get():
+#             self.quit.set(f"QUIT ({new_time})")
+#         self.root.after(100, self.quit_timer)
 
-config = Config()
+# def parse_key(event=None, key=None):
+#     if event is None:
+#         print(key)
+#         char = key
+#     else:
+#         char = event.char
 
-if config.get_option("darkmode", 0):
-    # Dark mode
-    bg = '#282828'
-    fg = '#d9d9d9'
-    fg_deselect = "#5f5f5f"
-else:
-    # LIGHT MODE
-    bg = '#ffffff'
-    fg = '#000000'
-    fg_deselect = "#a0a0a0"
+#     if char == "q":
+#         root.destroy()
 
-root = tk.Tk()
-root.title("quickscripts")
-app = MainMenu(root)
+#     elif char in "123456789":
+#         app.set_active_group(int(char))
 
-def parse_key(event=None, key=None):
-    if event is None:
-        print(key)
-        char = key
-    else:
-        char = event.char
+#     else:
+#         for script in app.scripts:
+#             if char == script.key:
+#                 script.execute()
+#                 root.destroy()
 
-    if char == "q":
-        root.destroy()
+class Api:
+    def __init__(self):
+        self.config = Config()
 
-    elif char in "123456789":
-        app.set_active_group(int(char))
+    def get_groups(self):
+        return [asdict(x) for x in self.config.groups]
 
-    else:
-        for script in app.scripts:
-            if char == script.key:
-                script.execute()
-                root.destroy()
+    def parse_key(self, key, shiftKey):
+        print(key, shiftKey)
+        # if shiftKey:
+        #     subprocess.Popen("thorium-browser https://youtube.com", shell=True)
+        #     print("Shift key is pressed")
 
-root.bind("<Key>", parse_key)
+    def close(self):
+        webview.windows[0].destroy()
 
-# WINDOWS & LINUX - forcing focus
-# root.wm_attributes("-topmost", 1)
-# root.focus_force()
+    def fit_window(self, width, height):
+        webview.windows[0].resize(width, height)
+        
+    def execute(self, command):
+        # subprocess.Popen(command.split(), shell=True)
+        subprocess.Popen(command, shell=True)
+        self.close()
+        
+    def show(self):
+        webview.windows[0].show()
 
-borderless = config.get_option('borderless', 0)
-if borderless == 1:
-    # LINUX
-    root.attributes('-type', 'dock')
-elif borderless == 2:
-    # WINDOWS
-    root.overrideredirect(True)
-
-root.mainloop()
+if __name__ == '__main__':
+    api = Api()
+    window = webview.create_window(
+        'Quickscripts',
+        'assets/index.html',
+        js_api=api,
+        min_size=(100, 100),
+        frameless=True,
+        on_top=True,
+        x=0,
+        y=0,
+        transparent=True
+    )
+    webview.start(debug=False)
